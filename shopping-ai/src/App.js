ea595488c213af87a6f3ed225ccef5776f89f14c
rajnada992 <rajnada1915@gmail.com>
2023-04-21 15:47:13 -0400

    removed unnecessary array

--- shopping-ai/src/App.js
+++ shopping-ai/src/App.js
@@ -1,20 +1,6 @@
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { useEffect, useState } from "react";
 
-const menuItems = [
-  { name: "Angus Burger", price: 8.99, category: "burger" },
-  { name: "Tuna Steak Burger", price: 15.0, category: "burger" },
-  { name: "Bacon Burger", price: 11.5, category: "burger" },
-  { name: "Southwest Chicken Burger", price: 9.99, category: "burger" },
-  { name: "Mozzarella Burger", price: 12.5, category: "burger" },
-  { name: "Cesar Salad", price: 6.5, category: "salad" },
-  { name: "BBQ Chicken Salad", price: 13.99, category: "salad" },
-  { name: "Garden Salad", price: 9.99, category: "salad" },
-  { name: "Veggie Lasagna", price: 17.99, category: "pasta" },
-  { name: "Spaghetti & Meatballs", price: 17.99, category: "pasta" },
-  { name: "Fettuccine Alfredo", price: 17.99, category: "pasta" },
-];
-
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
